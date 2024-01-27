6b1df4156ba968b0b0fadaa505522cda35cb3c8a
Donovan Hutchinson <donovanh@gmail.com>
2024-01-27 12:12:47 +0000

    Description

--- _data/metadata.js
+++ _data/metadata.js
@@ -2,7 +2,7 @@ module.exports = {
 	title: "Donovan's Vision Blog",
 	url: "https://vision.rodeo/",
 	language: "en",
-	description: "Learning how to build apps for the Apple Vision Pro.",
+	description: "Let's build great apps for Apple Vision Pro",
 	author: {
 		name: "Donovan Hutchinson",
 		email: "donovan@hop.ie",
